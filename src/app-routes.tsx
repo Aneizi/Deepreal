c24627ec16921b83fc37845723103399ec475a02
Muhammet Beyoglu <40030627+Aneizi@users.noreply.github.com>
2025-10-23 14:22:45 +0200

    fetch address and display

--- src/app-routes.tsx
+++ src/app-routes.tsx
@@ -1,5 +1,6 @@
 import { useRoutes } from 'react-router'
 import { lazy } from 'react'
+import AccountView from '@/components/account-view.tsx'
 
 const Dropzone = lazy(() => import('@/components/dropzone.tsx'))
 const AccountDetailFeature = lazy(() => import('@/features/account/account-feature-detail.tsx'))
@@ -8,6 +9,10 @@ const AccountIndexFeature = lazy(() => import('@/features/account/account-featur
 export function AppRoutes() {
   return useRoutes([
     { index: true, element: <Dropzone />},
+    {
+      path: 'view',
+      element: <AccountView/>
+    },
     {
       path: 'account',
       children: [

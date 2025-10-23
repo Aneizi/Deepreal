daa20080fd93518a99fa260e08cf0f884b48ffe9
Muhammet Beyoglu <40030627+Aneizi@users.noreply.github.com>
2025-10-23 10:15:30 +0200

    initial setup

--- src/app-routes.tsx
+++ src/app-routes.tsx
@@ -1,13 +1,13 @@
 import { useRoutes } from 'react-router'
 import { lazy } from 'react'
 
-const DashboardFeature = lazy(() => import('@/features/dashboard/dashboard-feature.tsx'))
+const Dropzone = lazy(() => import('@/components/dropzone.tsx'))
 const AccountDetailFeature = lazy(() => import('@/features/account/account-feature-detail.tsx'))
 const AccountIndexFeature = lazy(() => import('@/features/account/account-feature-index.tsx'))
 
 export function AppRoutes() {
   return useRoutes([
-    { index: true, element: <DashboardFeature /> },
+    { index: true, element: <Dropzone />},
     {
       path: 'account',
       children: [
